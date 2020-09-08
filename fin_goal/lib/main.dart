6ee94f103a081ca2d1b5afdccb2de126b3625cd9
Prerit Jain <prerit.jain@ad.infosys.com>
2020-09-08 23:23:41 +0530

    add button added

--- fin_goal/lib/main.dart
+++ fin_goal/lib/main.dart
@@ -20,21 +20,51 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
 
   List todos = List();
+  String input = "";
 
   @override
   void initState() {
     super.initState();
-    todos.add("Item1");
-    todos.add("Item2");
-    todos.add("Item3");
-    todos.add("Item4");
+    todos.add("Income1");
+    todos.add("Income2");
+    todos.add("Income3");
+    todos.add("Income4");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(
-  title: Text("myIncome")),
+  appBar: AppBar(
+  title: Text("Fin Goal"),
+  centerTitle: true),
+  floatingActionButton: FloatingActionButton(
+    onPressed: (){
+      showDialog(
+        context: context,
+        builder: (BuildContext context){
+          return AlertDialog(
+            title: Text("Add Income"),
+            content: TextField(
+              onChanged: (String value){
+                input = value;
+              },
+            ),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: (){
+                setState(() {
+                  todos.add(input);
+                });
+              }, child: Text("Add"))
+            ],
+          );});
+    }, 
+    child: Icon(
+      Icons.add,
+      color: Colors.white,
+    ),
+  ),
+  
   body: ListView.builder(
     itemCount: todos.length ,
     itemBuilder: (BuildContext context, int index){
