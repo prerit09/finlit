589a31a4e9b5c7b6b27a101ae3fcce43c1b61ac2
Prerit Jain <prerit.jain@ad.infosys.com>
2020-09-17 22:58:25 +0530

    firebase updates

--- fin_goal/lib/main.dart
+++ fin_goal/lib/main.dart
@@ -1,15 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          brightness: Brightness.light,
-          primaryColor: Colors.blue,
-          accentColor: Colors.orange),
-      home: MyApp(),
-    ));
+void main() async { 
 
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blue,
+        accentColor: Colors.orange),
+    home: MyApp(),
+  ));
+}
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
